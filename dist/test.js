cf1b31141c484e6fb0f2683cada74e054fa46c56
cdot <github@c-dot.co.uk>
2022-12-15 13:12:53 +0000

    D'oh - finally understood npm publish

--- dist/test.js
+++
@@ -1,7 +0,0 @@
-const { Encoder, Decoder } = require("./index.js");
-
-const data = [ "Sponge", "Tunicate", "Bryozoan" ];
-const frozen = Encoder.encode(data);
-const thawed = Decoder.decode(frozen);
-console.log(thawed);
-
